1680c3f71dbd62d20a5a12e2e3c3294f2c6eae7a
blueDange <672074694@qq.com>
2023-02-13 14:05:08 +0800

    fetch

--- day01/zhihuishequ/src/App03.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-import ParentClickContainer from './components/ParentClickContainer'
-
-export default class App extends Component {
-  render() {
-    return <ParentClickContainer></ParentClickContainer>
-  }
-}
